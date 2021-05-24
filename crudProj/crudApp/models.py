f420f8bd0e6f1e636030d29e253d8782d72072d8
dahye <smallgilr@gmail.com>
2021-05-25 03:41:44 +0900

    21-05-25-2

--- crudProj/crudApp/models.py
+++ crudProj/crudApp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
+    writer = models.CharField(null = False, max_length=15)
     body = models.TextField()
 
     def __str__(self):
